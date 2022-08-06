f28bc95238f664276db481fe9d090b06bc0b5634
Prasanna Balaji <erprasanna97@gmail.com>
2022-08-06 06:11:24 +0000

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,7 +4,10 @@ class Home extends Component {
 
     constructor(props) {
         super(props);
+        console.log("tst")
     }
+    
+    
   
     render() {
         return ( <h2 className="text-center">Home</h2>);
